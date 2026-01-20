af90a8d2ca6769afba071a5530b78c1ae3ac6ed3
Tristan Natvig <tristan.natvig@gmail.com>
2026-01-20 01:34:21 +0100

    Added a global value for the grid size so that it is scalable.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,19 @@
 
 // const gridBox =
 
+/*
+Sets up the 16 horizontal lines of the grid
+
+Assumes the flexGrid is already implemented in the DOM
+*/
+
+const GRIDSIZE = 16;
+
 function createGridLines(){
 
     let flexGrid = document.querySelector("#flexGrid")
     
-    for(i = 0; i < 16; i++){
+    for(let i = 0; i < GRIDSIZE; i++){
         console.log("made class");
         
         const gridLine = document.createElement("div");
@@ -13,11 +21,16 @@ function createGridLines(){
         flexGrid.appendChild(gridLine);
     }
 }
+/*
+Fills the lines with grid boxes
+
+Assumes the createGridLines is already called upon to set up the lines
+*/
 
 function createGridBoxes(){
-    let gridLine = document.querySelectorAll(".gridLine");
-    gridLine.forEach((line) => {
-        for(i = 0; i < 16; i++){
+    let gridLines = document.querySelectorAll(".gridLine");
+    gridLines.forEach((line) => {
+        for(let i = 0; i < GRIDSIZE; i++){
             const gridBox = document.createElement("div");
             gridBox.classList.add("gridBox");
             line.appendChild(gridBox);
@@ -25,6 +38,14 @@ function createGridBoxes(){
 
     })
 }
+
+
+
+
+
+
+
+
 
 createGridLines();
 createGridBoxes();

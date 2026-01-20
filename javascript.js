a386c18b36a1bfd9ddc8462d9b0e6dd44e0aeaab
Tristan Natvig <tristan.natvig@gmail.com>
2026-01-20 01:25:04 +0100

    Implenteded javascript functions for setting up the grid constisting of line boxes with set amount of horizontal boxes.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,30 @@
 
-function createGridLines
-const gridLine = document.createElement("div");
+// const gridBox =
+
+function createGridLines(){
+
+    let flexGrid = document.querySelector("#flexGrid")
+    
+    for(i = 0; i < 16; i++){
+        console.log("made class");
+        
+        const gridLine = document.createElement("div");
+        gridLine.classList.add("gridLine");
+        flexGrid.appendChild(gridLine);
+    }
+}
+
+function createGridBoxes(){
+    let gridLine = document.querySelectorAll(".gridLine");
+    gridLine.forEach((line) => {
+        for(i = 0; i < 16; i++){
+            const gridBox = document.createElement("div");
+            gridBox.classList.add("gridBox");
+            line.appendChild(gridBox);
+        }
+
+    })
+}
+
+createGridLines();
+createGridBoxes();
